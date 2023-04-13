31fb7eae395a532064ee66893e8b2d2a4b86a1fc
Yoel Arroyo <joel.arp89@gmail.com>
2023-04-13 12:15:56 +0200

    First Test: tablet dimensions

--- js/teaw_emotions.js
+++ js/teaw_emotions.js
@@ -39,56 +39,6 @@ var plantillasNodos = new go.Map();
 plantillasNodos.add("imagen", plantillaNodoImagen);
 diagram.nodeTemplateMap = plantillasNodos;
 
-
-// Plantilla Grupo de elementos
-diagram.groupTemplate =
-	$(go.Group, "Vertical",
-		{
-			// only allow those simple nodes that have the same data key prefix:
-			memberValidation: samePrefix,
-			// don't need to define handlers on member Nodes and Links
-			handlesDragDropForMembers: true,
-			// support highlighting of Groups when allowing a drop to add a member
-			mouseDragEnter: (e, grp, prev) => {
-				// this will call samePrefix; it is true if any node has the same key prefix
-				if (grp.canAddMembers(grp.diagram.selection)) {
-					var shape = grp.findObject("SHAPE");
-					if (shape)
-						shape.fill = "lightgreen";
-					grp.diagram.currentCursor = "";
-				} else {
-					var shape = grp.findObject("SHAPE");
-					if (shape)
-						shape.fill = "red";
-					grp.diagram.currentCursor = "not-allowed";
-				}
-			},
-			mouseDragLeave: (e, grp, next) => {
-				var shape = grp.findObject("SHAPE");
-				if (shape)
-					shape.fill = "rgba(239,239,240,1)";
-				grp.diagram.currentCursor = "";
-			},
-			// actually add permitted new members when a drop occurs
-			mouseDrop: (e, grp) => {
-				if (grp.canAddMembers(grp.diagram.selection)) {
-					// this will only add nodes with the same key prefix
-					grp.addMembers(grp.diagram.selection, true);
-				} else {  // and otherwise cancel the drop
-					grp.diagram.currentTool.doCancel();
-				}
-			}
-		},
-		// make sure all Groups are behind all regular Nodes
-		{ layerName: "Background" },
-		new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
-		$(go.Shape,
-			{
-				name: "SHAPE", width: 880, height: 640,
-				fill: "rgba(239,239,240,1)"
-			})
-	);
-
 // this predicate is true if both node data keys start with the same letter
 function samePrefix(group, node) {
 	if (group === null)
@@ -103,29 +53,91 @@ diagram.mouseDrop = e => {
 	diagram.commandHandler.addTopLevelParts(diagram.selection, true);
 };
 
-// El modelo del diagrama se encuentra vacío inicialmente
-diagram.model = $(go.GraphLinksModel,
-	{
-		linkKeyProperty: "key", // importante para poder compartir la información en un GraphLinksModel
-		nodeDataArray: [
-			{ key: "Correct", isGroup: true, loc: "0 -200" },
-			{ key: "Emotion", group: "Correct", source: "images/choice.png", desiredSize: new go.Size(256, 256), category: "imagen" },
-			//{ key: "Emotion", group: "Correct", loc: "350 -200", source: "images/choice.png", desiredSize: new go.Size(256, 256), category: "imagen" },
-			{ key: "Incorrect", loc: "128 500", source: "images/happy.png", desiredSize: new go.Size(128, 128), category: "imagen" },
-			{ key: "Correct", loc: "384 500", source: "images/quiet.png", desiredSize: new go.Size(128, 128), category: "imagen" },
-			{ key: "Incorrect", loc: "640 500", source: "images/sad.png", desiredSize: new go.Size(128, 128), category: "imagen" }
-		],
-		linkDataArray: []
-	});
+
+if (window.screen.width > 2560) {
+	initialGroupModelConfiguration(256, 1760, 1280, ["0 -200", "128 500", "384 500", "640 500"]);
+} else if (window.screen.width < 1280) {
+	initialGroupModelConfiguration(64, 440, 320, ["0 -100", "64 250", "192 250", "320 250"]);
+} else {
+	initialGroupModelConfiguration(128, 880, 640, ["0 -200", "128 500", "384 500", "640 500"]);
+}
+
+
+function initialGroupModelConfiguration(nodeSize, shapeWidth, shapeHeight, locations) {
+	// Plantilla Grupo de elementos
+	diagram.groupTemplate =
+		$(go.Group, "Vertical",
+			{
+				// only allow those simple nodes that have the same data key prefix:
+				memberValidation: samePrefix,
+				// don't need to define handlers on member Nodes and Links
+				handlesDragDropForMembers: true,
+				// support highlighting of Groups when allowing a drop to add a member
+				mouseDragEnter: (e, grp, prev) => {
+					// this will call samePrefix; it is true if any node has the same key prefix
+					if (grp.canAddMembers(grp.diagram.selection)) {
+						var shape = grp.findObject("SHAPE");
+						if (shape)
+							shape.fill = "lightgreen";
+						grp.diagram.currentCursor = "";
+					} else {
+						var shape = grp.findObject("SHAPE");
+						if (shape)
+							shape.fill = "red";
+						grp.diagram.currentCursor = "not-allowed";
+					}
+				},
+				mouseDragLeave: (e, grp, next) => {
+					var shape = grp.findObject("SHAPE");
+					if (shape)
+						shape.fill = "rgba(239,239,240,1)";
+					grp.diagram.currentCursor = "";
+				},
+				// actually add permitted new members when a drop occurs
+				mouseDrop: (e, grp) => {
+					if (grp.canAddMembers(grp.diagram.selection)) {
+						// this will only add nodes with the same key prefix
+						grp.addMembers(grp.diagram.selection, true);
+					} else {  // and otherwise cancel the drop
+						grp.diagram.currentTool.doCancel();
+					}
+				}
+			},
+			// make sure all Groups are behind all regular Nodes
+			{ layerName: "Background" },
+			new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
+			$(go.Shape,
+				{
+					name: "SHAPE", width: shapeWidth, height: shapeHeight,
+					fill: "rgba(239,239,240,1)"
+				})
+		);
+
+	// El modelo del diagrama se encuentra vacío inicialmente
+	diagram.model = $(go.GraphLinksModel,
+		{
+			linkKeyProperty: "key", // importante para poder compartir la información en un GraphLinksModel
+			nodeDataArray: [
+				{ key: "Correct", isGroup: true, loc: locations[0] },
+				{ key: "Emotion", group: "Correct", source: "images/choice.png", desiredSize: new go.Size(nodeSize * 2, nodeSize * 2), category: "imagen" },
+				//{ key: "Emotion", group: "Correct", loc: "350 -200", source: "images/choice.png", desiredSize: new go.Size(256, 256), category: "imagen" },
+				{ key: "Incorrect", loc: locations[1], source: "images/happy.png", desiredSize: new go.Size(nodeSize, nodeSize), category: "imagen" },
+				{ key: "Correct", loc: locations[2], source: "images/quiet.png", desiredSize: new go.Size(nodeSize, nodeSize), category: "imagen" },
+				{ key: "Incorrect", loc: locations[3], source: "images/sad.png", desiredSize: new go.Size(nodeSize, nodeSize), category: "imagen" }
+			],
+			linkDataArray: []
+		}
+	);
+}
 
 
 /***
 	COMUNICACIÓN CLIENTE-SERVIDOR --- TOGETHERJS ---
 ***/
-diagram.model.addChangedListener(function(e) {
+diagram.model.addChangedListener(function (e) {
 	if (e.isTransactionFinished) {
 		var json = e.model.toIncrementalJson(e);
-		
+
 		if (TogetherJS.running) {
 			TogetherJS.send({
 				type: "content-send",
@@ -136,7 +148,7 @@ diagram.model.addChangedListener(function(e) {
 	}
 });
 
-TogetherJS.hub.on("content-send", function(msg) {
+TogetherJS.hub.on("content-send", function (msg) {
 	if (!msg.sameUrl) {
 		return;
 	}
